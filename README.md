b8ea0d37f23c5ef8d7d060d926d8c32e103090a0
ederpbj <ederpbj@gmail.com>
2021-02-23 23:21:23 -0300

    Fim da Aula 1

--- README.md
+++ README.md
@@ -12,12 +12,17 @@ Projeto que cria uma rotina de exercícios, em um determinado tempo.
 
 [NLW](https://nextlevelweek.com/episodios/react/1/edicao/4?utm_source=convertkit&utm_medium=email&utm_campaign=NLW4+Aulas+dispon%C3%ADveis&utm_term=Leads+cadastrados&utm_content=Aula+01+React)
 
+[whimsical - criar fluxogramas](https://whimsical.com/MJrLar3fMC9vvTzVBxHMxZ)
+
+[Figma - Prototipar](https://www.figma.com/file/W62VId1hckJ2uMHfKg4XNj/Move.it-1.0-(Copy)?node-id=160%3A2761)
+
+[Fontes google](https://fonts.google.com/)
+
 [Aula 1](https://www.youtube.com/watch?v=XDFlV76UJuA&feature=emb_title&ab_channel=DiegoFernandes)
+
 
 ### `comandos cmd`
 
->> Criar projeto com template typescript
-
-`yarn create react-app moveit --template=typescript`
+`yarn create react-app moveit --template=typescript` >> Criar projeto com template typescript
 
 `yarn start` >> rodar o projeto
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ExperienceBar } from './components/ExperienceBar';
+import './styles/global.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <ExperienceBar />
     </div>
   );
 }
 
 export default App;
+
+
+/** Primeiro Teste
+ *
+ * import { Button } from './components/Button';
+ *  <div>
+      <h1>Olá React</h1>
+        <Button color="red" >
+          Botão 1
+        </Button>
+        <Button color="blue" >
+          Botão 2
+        </Button>
+        <Button color="green" >
+          Botão 3
+        </Button>
+
+    </div>
+ *
+
+ Segundo teste
+ <button style={{ height: '2rem' }}>BOTÃO</button>
+ */
